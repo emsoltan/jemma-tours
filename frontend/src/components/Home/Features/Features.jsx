5b2d316f02439e4ebaf0d9382b9eff8376fda0c4
emsoltan <e.mad.dragonfly.21@gmail.com>
2022-02-26 18:52:20 +0100

    added home/features section

--- frontend/src/components/Home/Features/Features.jsx
+++ frontend/src/components/Home/Features/Features.jsx
@@ -17,7 +17,8 @@ const Features = () => {
               Meet Nature
             </h3>
             <p className="feature-box__text">
-             Get in touch with our own nature, feel the peace & beauty of the world. Go somehwere where you can hear your inner thoughts away from our daily life.
+              Get in touch with our beautiful nature, go somehwere where you can
+              hear your inner thoughts & care for the peace & beauty of the world.
             </p>
           </div>
         </div>
@@ -27,29 +28,35 @@ const Features = () => {
             <img src={reset} className="feature-box__icon icon-basic-compass" />
             <h3 className="heading-tertiary u-margin-bottom-small">Reset it</h3>
             <p className="feature-box__text">
-             Leave all your troubles, heavy loads & luggage behind. Reset your mind & stop all the distractions, you will come back a lot lighter than when you started off.
+              Leave all your troubles, heavy loads & luggage behind. Reset your
+              mind & stop all the distractions, you will come back a lot lighter
+              than when you started off.
             </p>
           </div>
         </div>
 
         <div className="col-1-of-4">
           <div className="feature-box">
-            <img src={joy} className="feature-box__icon icon-basic-map"/>
+            <img src={joy} className="feature-box__icon icon-basic-map" />
             <h3 className="heading-tertiary u-margin-bottom-small">your Joy</h3>
             <p className="feature-box__text">
-           Find your joy in the purity of nature. Be it under a sea, atop the mountains or in a beautiful forest hike. You are always in control of your own happiness.
+              Find your joy in the purity of nature. Be it under a sea, atop the
+              mountains or in a beautiful forest hike. You are always in control
+              of your own happiness.
             </p>
           </div>
         </div>
 
         <div className="col-1-of-4">
           <div className="feature-box">
-            <img src={healthy} className="feature-box__icon icon-basic-heart"/>
+            <img src={healthy} className="feature-box__icon icon-basic-heart" />
             <h3 className="heading-tertiary u-margin-bottom-small">
               Live Healthy
             </h3>
             <p className="feature-box__text">
-             It is medically proven that our mental health level improves while spending time in nature. As well as countless physical health benefits. Mind, Body & Soul
+              It is medically proven that our mental health level improves while
+              spending time in nature. As well as countless physical health
+              benefits. Mind, Body & Soul
             </p>
           </div>
         </div>
