04cba6c20271457ddd5f8f05ea2a32e20e07920e
Mohammad Olfatee <m.olfatee@gmail.com>
2022-02-27 08:55:53 +0100

    BE Name edit

--- backend/public/js/bundle.js
+++ backend/public/js/bundle.js
@@ -9533,7 +9533,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "33229" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "39341" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
