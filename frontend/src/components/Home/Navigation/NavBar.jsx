a1e4aa6dae0a14b8777933f43252787f3ad563ef
emsoltan <e.mad.dragonfly.21@gmail.com>
2022-03-04 17:40:49 +0100

    front-end /cleaned/ homepage pure css

--- frontend/src/components/Home/Navigation/NavBar.jsx
+++ frontend/src/components/Home/Navigation/NavBar.jsx
@@ -3,7 +3,6 @@ import styled from "styled-components";
 import NavMenu from "./NavMenu";
 import logo from "../../../Assets/logo-final.svg";
 
-
 const Nav = styled.nav`
   width: 90%;
   margin: 0 auto;
@@ -13,17 +12,54 @@ const Nav = styled.nav`
   justify-content: space-between;
 
   .nav-logo {
-  max-height: 7rem;
-
+    max-height: 7rem;
   }
 `;
 
 const Navbar = () => {
   return (
-    <Nav>
-      <div className="logo"><img src={logo} alt="logo" className="nav-logo" /></div>
-      <NavMenu />
-    </Nav>
+    // <Nav>
+    //   <div className="logo"><img src={logo} alt="logo" className="nav-logo" /></div>
+    //   <NavMenu />
+    // </Nav>
+    <nav clasee="navbar">
+      <div className="navbar__logo">
+        <img src={logo} alt="logo" className="nav-logo" />
+      </div>
+      <div class="navbar__menu">
+        {/* BURGER MENU GOES HERE = top */}
+        {/* <StyledBurger open={open} onClick={() => setOpen(!open)} class="navbar__burger">
+          <div class="burger-slice" />
+          <div />
+          <div />
+        </StyledBurger> */}
+
+        {/* NAVIGATION LINKS GO HERE = bottom */}
+        <ul open={open} class="navbar__list">
+          <li>
+            <a href="/" onClick={() => setOpen(!open)}>
+              Home
+            </a>
+          </li>
+
+          <li>
+            <a href="/tours" onClick={() => setOpen(!open)}>
+              Tours
+            </a>
+          </li>
+          <li>
+            <a href="/login" onClick={() => setOpen(!open)}>
+              Login
+            </a>
+          </li>
+          <li>
+            <a href="/signup" onClick={() => setOpen(!open)}>
+              signup
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
   );
 };
 
